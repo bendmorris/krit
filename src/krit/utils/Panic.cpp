49b8d428493112ccf2f9e3b0f51e4d08e1671f93
Ben Morris <ben@bendmorris.com>
2026-06-20 12:18:41 -0700

    wtf is vpanic doing? just print to stderr

--- src/krit/utils/Panic.cpp
+++ src/krit/utils/Panic.cpp
@@ -9,14 +9,11 @@ void panic(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     vpanic(fmt, args);
+    va_end(args);
 }
 
 void vpanic(const char *fmt, va_list args) {
     vfprintf(stderr, fmt, args);
-    fputs("\n", stderr);
-    ssize_t len = vsnprintf(NULL, 0, fmt, args);
-    char *buf = static_cast<char *>(std::malloc(len + 1));
-    vsnprintf(buf, len + 1, fmt, args);
     std::abort();
 }
 
